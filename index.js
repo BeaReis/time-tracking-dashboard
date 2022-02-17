fa4ed86f6ffc0fc9f052b60668d74ded920190f4
BeaReis <pcreis.beatriz@gmail.com>
2022-02-17 15:40:56 -0300

    Fetch data and set it to text

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
+
 const button = $('button');
+
+function hoursFormat(hours) {
+    if (hours === 1) {
+        return "hr"
+    } else {
+        return "hrs"
+    }
+}
+
+function previousFormat(previous) {
+    if (previous === 'daily') {
+        return "Yesterday - "
+    } else if (previous === 'weekly') {
+        return "Last week - "
+    } else {
+        return "Last month - "
+    }
+}
 
 /* Trigger a function when a button is clicked */
 button.click(function(event) { 
@@ -16,12 +35,42 @@ button.click(function(event) {
         if (this.id !== clickedBtn) {
             $(`#${this.id}`).removeClass('selectedBtn');
         } else {
+            /* Fetches the data from json file */
             fetch('data.json')
             .then((res) => res.json())
             .then((data) => {
-                console.log(data[key])
-        });
+                data.forEach(element => {
+
+                    var hoursDataCurrent = element.timeframes[clickedBtn].current
+                    var hoursDataPrevious = element.timeframes[clickedBtn].previous
+
+                    switch(element.title) {
+                        case "Work": 
+                            $('#work h1').text(hoursDataCurrent + hoursFormat(hoursDataCurrent)); 
+                            $('#work p').text(previousFormat(clickedBtn) + hoursDataPrevious + hoursFormat(hoursDataCurrent)); 
+                            break;
+                        case "Play": 
+                            $('#play h1').text(hoursDataCurrent + hoursFormat(hoursDataCurrent));
+                            $('#play p').text(previousFormat(clickedBtn) + hoursDataPrevious + hoursFormat(hoursDataPrevious));
+                            break;
+                        case "Study": 
+                            $('#study h1').text(hoursDataCurrent + hoursFormat(hoursDataCurrent));
+                            $('#study p').text(previousFormat(clickedBtn) + hoursDataPrevious + hoursFormat(hoursDataPrevious));
+                            break;
+                        case "Exercise": 
+                            $('#exercise h1').text(hoursDataCurrent + hoursFormat(hoursDataCurrent));
+                            $('#exercise p').text(previousFormat(clickedBtn) + hoursDataPrevious + hoursFormat(hoursDataPrevious));
+                            break;
+                        case "Social": 
+                            $('#social h1').text(hoursDataCurrent + hoursFormat(hoursDataCurrent));
+                            $('#social p').text(previousFormat(clickedBtn) + hoursDataPrevious + hoursFormat(hoursDataPrevious));
+                            break;
+                        default: 
+                            $('#selfCare h1').text(hoursDataCurrent + hoursFormat(hoursDataCurrent));
+                            $('#selfCare p').text(previousFormat(clickedBtn) + hoursDataPrevious + hoursFormat(hoursDataPrevious));
+                    }
+                });
+            });
         }
     });
-   
 });
